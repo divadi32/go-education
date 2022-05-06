ecda185813da09f8c9fead4c2c91e346413be997
divadi32 <vadimkravchuk67@gmail.com>
2022-05-06 18:55:15 +0300

    Added Benchmark to file main_test.go

--- trainee-tester/test-http-server/main_test.go
+++ trainee-tester/test-http-server/main_test.go
@@ -23,3 +23,22 @@ func TestHelloHandler(t *testing.T) {
 		)
 	}
 }
+
+func BenchmarkHelloHandler(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		wr := httptest.NewRecorder()
+		req := httptest.NewRequest(http.MethodGet, "/hello", nil)
+
+		helloHandler(wr, req)
+		if wr.Code != http.StatusOK {
+			b.Errorf("got HTTP status code %d, expected 200", wr.Code)
+		}
+
+		if !strings.Contains(wr.Body.String(), "Hello") {
+			b.Errorf(
+				`response body "%s" does not contain "Hello"`,
+				wr.Body.String(),
+			)
+		}
+	}
+}
