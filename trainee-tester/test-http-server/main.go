8c6bb604776582e8ed751aee90e5c0b2a18ba938
divadi32 <vadimkravchuk67@gmail.com>
2022-05-06 17:28:11 +0300

    added http server to project test-http-server

--- trainee-tester/test-http-server/main.go
+++ trainee-tester/test-http-server/main.go
@@ -2,12 +2,16 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello")
+}
+
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello")
-	})
+	http.HandleFunc("/hello", helloHandler)
+	log.Println("Listening...")
 	http.ListenAndServe(":8080", nil)
 }
