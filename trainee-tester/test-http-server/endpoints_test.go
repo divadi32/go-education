55917ceaeb0b7b05117637adf341393b438b6af5
LeeDark <sergey.povarnin@gmail.com>
2022-05-18 17:25:16 +0300

    Test HTTP Server: renamed and updated tests.

---
+++ trainee-tester/test-http-server/endpoints_test.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+)
+
+func TestPingHandler(t *testing.T) {
+	wr := httptest.NewRecorder()
+	req := httptest.NewRequest(http.MethodGet, "/ping", nil)
+
+	pingHandler(wr, req)
+	if wr.Code != http.StatusOK {
+		t.Errorf("got HTTP status code %d, expected 200", wr.Code)
+	}
+
+	if !strings.Contains(wr.Body.String(), "pong") {
+		t.Errorf(
+			`response body "%s" does not contain "NAME"`,
+			wr.Body.String(),
+		)
+	}
+}
+
+func BenchmarkPingHandler(b *testing.B) {
+	wr := httptest.NewRecorder()
+	req := httptest.NewRequest(http.MethodGet, "/ping", nil)
+
+	pingHandler(wr, req)
+}
+
+func TestHelloHandler(t *testing.T) {
+	wr := httptest.NewRecorder()
+	req := httptest.NewRequest(http.MethodGet, "/hello?name=Vadim", nil)
+
+	helloHandler(wr, req)
+	if wr.Code != http.StatusOK {
+		t.Errorf("got HTTP status code %d, expected 200", wr.Code)
+	}
+
+	if !strings.Contains(wr.Body.String(), "Vadim") {
+		t.Errorf(
+			`response body "%s" does not contain "NAME"`,
+			wr.Body.String(),
+		)
+	}
+}
+
+func BenchmarkHelloHandler(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		wr := httptest.NewRecorder()
+		req := httptest.NewRequest(http.MethodGet, "/hello?name=NAME", nil)
+
+		helloHandler(wr, req)
+	}
+}
+
+// func TestTimeoutHandler(t *testing.T) {
+// 	wr := httptest.NewRecorder()
+// 	req := httptest.NewRequest(http.MethodGet, "/timeout", nil)
+
+// 	timeoutHandler(wr, req)
+// 	if wr.Code != http.StatusOK {
+// 		t.Errorf("got HTTP status code %d, expected 200", wr.Code)
+// 	}
+
+// 	if !strings.Contains(wr.Body.String(), "NAME_timeout") {
+// 		t.Errorf(
+// 			`response body "%s" does not contain "NAME_timeout"`,
+// 			wr.Body.String(),
+// 		)
+// 	}
+// }
+
+// func BenchmarkTimeoutHandler(b *testing.B) {
+// 	wr := httptest.NewRecorder()
+// 	req := httptest.NewRequest(http.MethodGet, "/timeout", nil)
+
+// 	timeoutHandler(wr, req)
+// 	if wr.Code != http.StatusOK {
+// 		b.Errorf("got HTTP status code %d, expected 200", wr.Code)
+// 	}
+
+// 	if !strings.Contains(wr.Body.String(), "NAME_timeout") {
+// 		b.Errorf(
+// 			`response body "%s" does not contain "NAME_timeout"`,
+// 			wr.Body.String(),
+// 		)
+// 	}
+// }
