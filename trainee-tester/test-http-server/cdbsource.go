e463796569904f9a88e8614652502316b7fb426d
divadi32 <vadimkravchuk67@gmail.com>
2022-05-28 17:21:09 +0300

    added comments with cdb source parameters

--- trainee-tester/test-http-server/cdbsource.go
+++ trainee-tester/test-http-server/cdbsource.go
@@ -2,6 +2,13 @@ package main
 
 import "net/http"
 
+//CdbSourceResponse is struct for /csbsource endpoint response
+
 func cdbSourceHeandler(w http.ResponseWriter, r *http.Request) {
+	// request: /cdbsource?name=akton&number=123456789
+
+	// TODO: logic
+
+	// response: JSON {"number": "123456789", "mcc": MCC, "mnc": MNC, "ported": isPortede }
 	sendJSON(w, map[string]string{"answer": "pong"})
 }
